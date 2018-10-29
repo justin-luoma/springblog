0a27a9a06764b86eea588cc1e230c73fb085f63d
Justin Luoma <justin@justinluoma.com>
2018-10-29 18:23:29 -0500

    Spring Fundamentals: Views: roll-dice complete

--- src/main/java/com/codeup/springblog/MathController.java
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -1,4 +1,4 @@
-package com.justinluoma.springblog;
+package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -0,0 +1,24 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String index() {
+        return "roll";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String guess(@PathVariable int n, Model model) {
+        int random = (int )(Math.random() * 6 + 1);
+        model.addAttribute("roll", random);
+        model.addAttribute("guess", n);
+
+        return "guess";
+    }
+}
