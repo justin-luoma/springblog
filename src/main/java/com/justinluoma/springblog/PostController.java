fd483cd13f850c4ad577330a3d15c938a498358e
Justin Luoma <justin@justinluoma.com>
2018-10-29 10:42:00 -0500

    Spring Fundamentals: Controllers

---
+++ src/main/java/com/justinluoma/springblog/PostController.java
@@ -0,0 +1,35 @@
+package com.justinluoma.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String postsPage() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postPage(@PathVariable int id) {
+        return "view an individual post" + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPage() {
+        return "view the form for creating a post" ;
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String create() {
+        return "create a new post";
+    }
+}
