949c1d79d823de42b74e7b79f0258d2ff19ba47f
Justin Luoma <justin@justinluoma.com>
2018-10-30 08:24:15 -0500

    Spring Fundamentals: Views

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,24 +1,36 @@
 package com.codeup.springblog;
 
+import com.codeup.springblog.dao.model.Post;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String postsPage() {
-        return "posts index page";
+    public String postsPage(Model model) {
+
+        List<Post> posts = Arrays.asList(
+                new Post("Post #1", "Content for post 1"),
+                new Post("Post #2", "Content for post 2")
+        );
+
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postPage(@PathVariable int id) {
-        return "view an individual post" + id;
+    public String postPage(@PathVariable int id, Model model) {
+        model.addAttribute("post", new Post("Single Page Post Title", "Post body content."));
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
